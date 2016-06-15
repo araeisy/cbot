4fece2aacef6cdc5df4b90ce1262d5568cbd7dd0
emandaneshikohan@gmail.com <emandaneshikohan@gmail.com>
2016-06-15 11:03:23 -0400

    Added add_folder,setup_user,del_user functions

--- utils.py
+++ utils.py
@@ -18,6 +18,63 @@ pool = redis.ConnectionPool(host='localhost', port=6379, db=0, password=None,dec
 db = redis.Redis(connection_pool=pool)
 bot = telepot.Bot(config.TOKEN)
 
+def add_folder(msg, name, previous_folder):
+    user_id = msg["from"]["id"]
+    folder_num = 0
+    user_folders = db.scard("user:" + str(user_id) + ":added_folders")
+    if user_folders:
+        folder_num = user_folders
+    folder_id = str(user_id) + "_" + str(folder_num)
+    db.sadd("folders", folder_id)
+    db.sadd("user:" + str(user_id) + ":added_folders", folder_id)
+    db.sadd("user:" + str(user_id) + ":folders", folder_id)
+    db.hset("folder:" + folder_id, "num", folder_num)
+    db.hset("folder:" + folder_id, "name", name)
+    db.hset("folder:" + folder_id, "owner", user_id)
+    db.hset("folder:" + folder_id, "time_created", int(time.time()))
+    if previous_folder:
+        db.sadd("folder_subs:" + previous_folder, folder_id)
+        db.hset("folder:" + folder_id, "previous_folder", previous_folder)
+    return folder_id
+
+
+def setup_user(msg):
+    user_id = msg["from"]["id"]
+    db.sadd("started_users", user_id)
+    db.hset("users:" +str(user_id), "setup_time", int(time.time()))
+    db.hset("users:" +str(user_id), "working_directory", str(user_id) + "_0")
+    add_folder(msg, "root", False)
+    default_folders = ["Audio", "Video", "Voice", "Text", "Sticker", "Contact", "File"]
+    for i in default_folders:
+        add_folder(msg, i, str(user_id) + "_0")
+    return True
+
+def get_folders_text(folder):
+    folders = db.smembers("folder_subs:" + folder)
+    if folders:
+        text = ""
+        for k in folders:
+            folder_name = db.hget("folder:" + k, "name")
+            print(folder_name)
+            text += "— " + folder_name + " \n"
+        return text
+    else:
+        return False
+def del_user(msg):
+    user_id = str(msg["from"]["id"])
+    db.srem("started_users", user_id)
+    folders_list = db.smembers("user:" + user_id + ":added_folders")
+    if folders_list:
+        folders_list = list(folders_list)
+        for k in folders_list:
+            db.srem("folders", k)
+            db.delete("folder:" + k)
+            db.delete("folder_subs:" + k)
+    db.delete("user:" + user_id + ":added_folders")
+    db.delete("user:" + user_id + ":folders")
+    db.delete("users:" + user_id)
+    return True
+
 def markdown_escape(text):
     text = text.replace("_", "\\_")
     text = text.replace("[", "\\{")
